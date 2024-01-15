bf543fe581fb11f1a20f0279c23648970bfa69d9
HachimEnnaji <ashennaji@hotmail.it>
2024-01-15 04:15:22 +0100

    link to homepage

--- src/components/TitlePage.jsx
+++ src/components/TitlePage.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 function TitlePage() {
   return (
     <Navbar bg="light" className="mb-5 navbar">
-      <Navbar.Brand href="#">
+      <Navbar.Brand as={Link} to="/">
         <h2 className="text-white">Meteo Weather</h2>
       </Navbar.Brand>
     </Navbar>
