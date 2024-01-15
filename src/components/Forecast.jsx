782de8fd8881adf495b9dd5d0469d72dd401d389
HachimEnnaji <ashennaji@hotmail.it>
2024-01-15 04:08:09 +0100

    need to add changing background weather image in forecast page and add details city

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -17,15 +17,15 @@ function Forecast({ place }) {
   const [imageBackground, setImageBackground] = useState("");
   const [urlAnime, setUrlAnime] = useState("");
 
-  const thunderstorm200 = "https://i.ytimg.com/vi/wF6DoQn6pxU/maxresdefault.jpg";
-  const drizzle300 =
-    "https://www.metoffice.gov.uk/binaries/content/gallery/metofficegovuk/hero-images/weather/rain/raindrops-misted-on-a-windscreen.jpg";
-  const rain500 = "https://i.ytimg.com/vi/P-7Lwh93vDI/maxresdefault.jpg";
-  const snow600 = "https://i.ytimg.com/vi/MYnElTvpMPY/maxresdefault.jpg";
-  const fog700 = "https://c4.wallpaperflare.com/wallpaper/370/629/227/anime-original-fog-girl-hd-wallpaper-thumb.jpg";
-  const clear800d = "https://wallpapercave.com/wp/wp8383953.jpg";
-  const clear800n = "https://as1.ftcdn.net/v2/jpg/05/51/75/12/1000_F_551751216_AQtvKSsd37KBYdLwJ9lPWGqId4zfMBnZ.jpg";
-  const cloud801 = "https://c4.wallpaperflare.com/wallpaper/842/18/565/anime-sky-cloud-clouds-wallpaper-preview.jpg";
+  //   const thunderstorm200 = "https://i.ytimg.com/vi/wF6DoQn6pxU/maxresdefault.jpg";
+  //   const drizzle300 =
+  //     "https://www.metoffice.gov.uk/binaries/content/gallery/metofficegovuk/hero-images/weather/rain/raindrops-misted-on-a-windscreen.jpg";
+  //   const rain500 = "https://i.ytimg.com/vi/P-7Lwh93vDI/maxresdefault.jpg";
+  //   const snow600 = "https://i.ytimg.com/vi/MYnElTvpMPY/maxresdefault.jpg";
+  //   const fog700 = "https://c4.wallpaperflare.com/wallpaper/370/629/227/anime-original-fog-girl-hd-wallpaper-thumb.jpg";
+  //   const clear800d = "https://wallpapercave.com/wp/wp8383953.jpg";
+  //   const clear800n = "https://as1.ftcdn.net/v2/jpg/05/51/75/12/1000_F_551751216_AQtvKSsd37KBYdLwJ9lPWGqId4zfMBnZ.jpg";
+  //   const cloud801 = "https://c4.wallpaperflare.com/wallpaper/842/18/565/anime-sky-cloud-clouds-wallpaper-preview.jpg";
 
   const timeConverter = (data) => {
     let a = new Date(data * 1000);
@@ -38,31 +38,31 @@ function Forecast({ place }) {
     return [date, hour, `${date} ${month}`];
   };
 
-  const getBackgroundImageUrlAnime = ({ id, icon }) => {
-    let backgroundImageUrl;
+  //   const getBackgroundImageUrlAnime = ({ id, icon }) => {
+  //     let backgroundImageUrl;
 
-    // Confronto con l'ID del tempo per selezionare l'immagine di sfondo appropriata
-    if (id >= 200 && id <= 232) {
-      backgroundImageUrl = thunderstorm200;
-    } else if (id >= 300 && id <= 321) {
-      backgroundImageUrl = drizzle300;
-    } else if (id >= 500 && id <= 531) {
-      backgroundImageUrl = rain500;
-    } else if (id >= 600 && id <= 622) {
-      backgroundImageUrl = snow600;
-    } else if (id >= 701 && id <= 781) {
-      backgroundImageUrl = fog700;
-    } else if (id === 800) {
-      if (icon === "01d") {
-        backgroundImageUrl = clear800d;
-      } else {
-        backgroundImageUrl = clear800n;
-      }
-    } else if (id >= 801 && id <= 804) {
-      backgroundImageUrl = cloud801;
-    }
-    setUrlAnime(backgroundImageUrl);
-  };
+  //     // Confronto con l'ID del tempo per selezionare l'immagine di sfondo appropriata
+  //     if (id >= 200 && id <= 232) {
+  //       backgroundImageUrl = thunderstorm200;
+  //     } else if (id >= 300 && id <= 321) {
+  //       backgroundImageUrl = drizzle300;
+  //     } else if (id >= 500 && id <= 531) {
+  //       backgroundImageUrl = rain500;
+  //     } else if (id >= 600 && id <= 622) {
+  //       backgroundImageUrl = snow600;
+  //     } else if (id >= 701 && id <= 781) {
+  //       backgroundImageUrl = fog700;
+  //     } else if (id === 800) {
+  //       if (icon === "01d") {
+  //         backgroundImageUrl = clear800d;
+  //       } else {
+  //         backgroundImageUrl = clear800n;
+  //       }
+  //     } else if (id >= 801 && id <= 804) {
+  //       backgroundImageUrl = cloud801;
+  //     }
+  //     setUrlAnime(backgroundImageUrl);
+  //   };
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -131,34 +131,32 @@ function Forecast({ place }) {
   }
 
   return (
-    <Container className="background-city" style={`url(${urlAnime})`}>
+    <Container className="background-city">
       <div className="forecast-format text-white ">
         <h2>{` ${place.name}  ${place.state ? "• " + place.state : ""} `}</h2>
         {continent ? <h3>{continent}</h3> : <></>}
       </div>
       <Carousel>
         {forecastData.list.map((dayForecast, index) => (
-          <>
-            getBackgroundImageUrlAnime(dayForecast.weather[0]);
-            <Carousel.Item key={`dayForecast${index}`} className="my-3">
-              <img src={imageBackground} alt="city" className="rounded-2 img-forecast" />
-              <Carousel.Caption>
-                <div className="forecast-format">
-                  <h3>
-                    <MdCalendarToday /> {timeConverter(dayForecast.dt)[2]} <TbClockHour9 />{" "}
-                    {timeConverter(dayForecast.dt)[1]} : 00
-                  </h3>
-                  <img src={`https://openweathermap.org/img/wn/${dayForecast.weather[0].icon}@2x.png`} alt="" />
-                  <h4>
-                    <TbTemperature /> {Math.round(dayForecast.main.temp)} <RiCelsiusFill />
-                  </h4>
-                  <h4>
-                    <WiHumidity /> {dayForecast.main.humidity} % - <GiWindsock /> {dayForecast.wind.speed} Km/h
-                  </h4>
-                </div>
-              </Carousel.Caption>
-            </Carousel.Item>
-          </>
+          // getBackgroundImageUrlAnime(dayForecast.weather[0]);
+          <Carousel.Item key={`dayForecast${index}`} className="my-3">
+            <img src={imageBackground} alt="city" className="rounded-2 img-forecast" />
+            <Carousel.Caption>
+              <div className="forecast-format">
+                <h3>
+                  <MdCalendarToday /> {timeConverter(dayForecast.dt)[2]} <TbClockHour9 />{" "}
+                  {timeConverter(dayForecast.dt)[1]} : 00
+                </h3>
+                <img src={`https://openweathermap.org/img/wn/${dayForecast.weather[0].icon}@2x.png`} alt="" />
+                <h4>
+                  <TbTemperature /> {Math.round(dayForecast.main.temp)} <RiCelsiusFill />
+                </h4>
+                <h4>
+                  <WiHumidity /> {dayForecast.main.humidity} % - <GiWindsock /> {dayForecast.wind.speed} Km/h
+                </h4>
+              </div>
+            </Carousel.Caption>
+          </Carousel.Item>
         ))}
       </Carousel>
 
