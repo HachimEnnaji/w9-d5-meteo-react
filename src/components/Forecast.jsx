e0ad28e58a288a2185d7a6b92097d9a114434857
HachimEnnaji <ashennaji@hotmail.it>
2025-03-25 23:07:10 +0100

    add city fetch

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Carousel, Container, Spinner } from "react-bootstrap";
 import { useParams } from "react-router";
 import { MdCalendarToday } from "react-icons/md";
@@ -13,11 +13,9 @@ const TOKEN = "4c67e6677a48d9f7e61fcbff458ccff4";
 function Forecast({ place }) {
   const { lat, lon, city } = useParams();
 
-  const { data, error, fetchData, loading } = UseFetch(
+  const { data, error, loading } = UseFetch(
     `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${TOKEN}&units=metric`
   );
-
-  const [imageBackground, setImageBackground] = useState("");
 
   const timeConverter = (data) => {
     let a = new Date(data * 1000);
@@ -28,10 +26,6 @@ function Forecast({ place }) {
     if (hour < 9) hour = "0" + hour;
     return [date, hour, `${date} ${month}`];
   };
-
-  useEffect(() => {
-    fetchData();
-  }, []); // Assicura il fetch dei dati
 
   if (loading) {
     return <Spinner animation="grow" variant="success" />;
@@ -44,34 +38,32 @@ function Forecast({ place }) {
   if (!data || !data.list) {
     return <p>Nessun dato disponibile.</p>;
   }
-
+  if (data) {
+    console.log(data);
+  }
   return (
     <Container className="background-city">
       <div className="forecast-format text-white ">
         <h2>{` ${place?.name || city}  ${place?.state ? "• " + place.state : ""} `}</h2>
       </div>
-      <Carousel>
-        {data.list.map((dayForecast, index) => (
-          <Carousel.Item key={`dayForecast${index}`} className="my-3">
-            <img src={imageBackground} alt="city" className="rounded-2 img-forecast" />
-            <Carousel.Caption>
-              <div className="forecast-format">
-                <h3>
-                  <MdCalendarToday /> {timeConverter(dayForecast.dt)[2]} <TbClockHour9 />{" "}
-                  {timeConverter(dayForecast.dt)[1]}:00
-                </h3>
-                <img src={`https://openweathermap.org/img/wn/${dayForecast.weather[0].icon}@2x.png`} alt="" />
-                <h4>
-                  <TbTemperature /> {Math.round(dayForecast.main.temp)} <RiCelsiusFill />
-                </h4>
-                <h4>
-                  <WiHumidity /> {dayForecast.main.humidity} % - <GiWindsock /> {dayForecast.wind.speed} Km/h
-                </h4>
-              </div>
-            </Carousel.Caption>
-          </Carousel.Item>
+      {data &&
+        data.list.map((dayForecast, index) => (
+          <div key={`dayForecast${index}`} className="my-3">
+            <div className="forecast-format">
+              <h3>
+                <MdCalendarToday /> {timeConverter(dayForecast.dt)[2]} <TbClockHour9 />{" "}
+                {timeConverter(dayForecast.dt)[1]}:00
+              </h3>
+              <img src={`https://openweathermap.org/img/wn/${dayForecast.weather[0].icon}@2x.png`} alt="" />
+              <h4>
+                <TbTemperature /> {Math.round(dayForecast.main.temp)} <RiCelsiusFill />
+              </h4>
+              <h4>
+                <WiHumidity /> {dayForecast.main.humidity} % - <GiWindsock /> {dayForecast.wind.speed} Km/h
+              </h4>
+            </div>
+          </div>
         ))}
-      </Carousel>
     </Container>
   );
 }
--- src/hooks/fetch.hook.jsx
+++ src/hooks/fetch.hook.jsx
@@ -5,26 +5,25 @@ export default function useFetch(url) {
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true); // Inizialmente true
 
-  const fetchData = async () => {
-    setLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      const json = await response.json();
-      setData(json);
-    } catch (err) {
-      setError(err);
-    } finally {
-      setLoading(false);
-    }
-  };
-
   useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      setError(null);
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const json = await response.json();
+        setData(json);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    };
     fetchData();
   }, [url]); // Effettua il fetch al mount o se cambia l'URL
 
-  return { data, error, loading, fetchData };
+  return { data, error, loading };
 }
